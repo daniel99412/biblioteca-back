f8aba05b488684c981f8729ba8448b8f9c322e41
Daniel Pardo <daniel.pardo194@gmail.com>
2022-02-24 12:31:44 -0600

    serverConfigAndConnectionToDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DB, DB_HOST, DB_PORT, DB_PWD, DB_USER } from './config/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>(DB_HOST),
+        port: +configService.get<number>(DB_PORT),
+        username: configService.get<string>(DB_USER),
+        password: configService.get<string>(DB_PWD),
+        database: configService.get<string>(DB),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const SERVER_PORT = 'SERVER_PORT';
+export const DB_HOST = 'DB_HOST';
+export const DB_PORT = 'DB_PORT';
+export const DB_USER = 'DB_USER';
+export const DB_PWD = 'DB_PWD';
+export const DB = 'DB';
